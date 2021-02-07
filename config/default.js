9608160453c9f09b58d851c9c09bca886bf59eeb
eugene@git config --global user.name eugenegit config --global user.name eugene@ <eugnazaroff@gmail.com>
2021-02-08 00:03:34 +0400

    created base server connected to mongodb

---
+++ config/default.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI:
+        'mongodb+srv://admin:admin@cluster0.wxrrd.mongodb.net/dev-connector?retryWrites=true&w=majority',
+}
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const config = require('config')
 const authRouter = require('./routes/api/auth')
 const usersRouter = require('./routes/api/users')
 const profileRouter = require('./routes/api/profile')
 const postsRouter = require('./routes/api/posts')
+const mongoose = require('mongoose')
 const app = express()
 
 app.use(express.json())
@@ -11,3 +13,19 @@ app.use('/api/auth', authRouter)
 app.use('/api/users', usersRouter)
 app.use('/api/posts', profileRouter)
 app.use('/api/profile', postsRouter)
+
+const connectToDatabase = async () => {
+    try {
+        await mongoose.connect(config.get('mongoURI'), {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        app.listen(process.env.PORT || 5000, () => {
+            console.log('server started successflly')
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+connectToDatabase()
